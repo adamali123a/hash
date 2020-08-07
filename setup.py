776f8ec146c71ba51de85cd9ab9337a822ee4dfe
adamali123a <54318139+adamali123a@users.noreply.github.com>
2020-08-07 09:41:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ import time
 time.sleep(2)
 os.system("pip install base64")
 os.system("pip install hashlib")
+os.system("apt update")
+os.system("apt upgrade -y")
 time.sleep(4)
 os.system("rm -rif setup.py")
